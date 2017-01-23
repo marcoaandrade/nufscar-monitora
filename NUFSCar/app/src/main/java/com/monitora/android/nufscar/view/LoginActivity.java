96f2e091510c205112f0257bb57c77035f5c67cd
Joao Marins <jgcmarins@gmail.com>
2017-01-23 21:27:29 -0200

    activities created

--- NUFSCar/app/src/main/java/com/monitora/android/nufscar/view/LoginActivity.java
+++ NUFSCar/app/src/main/java/com/monitora/android/nufscar/view/LoginActivity.java
@@ -1,13 +1,15 @@
-package com.monitora.android.nufscar;
+package com.monitora.android.nufscar.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.monitora.android.nufscar.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
