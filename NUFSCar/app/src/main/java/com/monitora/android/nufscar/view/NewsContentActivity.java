fe92b474b0225b410b5f076dd3ebc82ea570d468
Marco Andrade <Marco Andrade>
2017-01-24 17:25:30 -0200

    Alteracao no layout das Activities

---
+++ NUFSCar/app/src/main/java/com/monitora/android/nufscar/view/NewsContentActivity.java
@@ -0,0 +1,13 @@
+package com.monitora.android.nufscar.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewsContentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_content);
+    }
+}
